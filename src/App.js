946163267751d2489106fa32353e5ff194c5de0c
Asutosh Pradhan <20ec3014@rgipt.ac.in>
2021-10-25 13:28:07 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import './App.css';
-
+import Navbar from './Components/Navbar';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       
-      </header>
-    </div>
+    <Navbar/>
   );
 }
 
---
+++ src/Navbar.jsx
@@ -0,0 +1,15 @@
+
+import Navbar from 'react-bootstrap/Navbar'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container'
+
+export default function Navbar() {
+    return (
+        <>
+            asutnxc
+        </>
+
+    )
+}
+
