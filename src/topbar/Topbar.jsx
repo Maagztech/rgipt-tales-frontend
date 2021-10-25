305ba2ed218697ad8d99720994c8e8e90146ebf3
Harsha1309 <harshyadav1393@gmail.com>
2021-10-25 23:13:41 +0530

    Bootstrap problem solved

--- src/topbar/Topbar.jsx
+++ src/topbar/Topbar.jsx
@@ -1,43 +1,44 @@
+import React from 'react'
 import './Topbar.css'
-
 export default function Topbar() {
-    return (
-        <nav ClassNameName="navbar navbar-expand-lg navbar-light bg-light">
-        <div ClassName="container-fluid">
-          <a ClassName="navbar-brand" href="#">Navbar</a>
-          <button ClassName="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span ClassName="navbar-toggler-icon"></span>
-          </button>
-          <div ClassName="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul ClassName="navbar-nav me-auto mb-2 mb-lg-0">
-              <li ClassName="nav-item">
-                <a ClassName="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li ClassName="nav-item">
-                <a ClassName="nav-link" href="#">Link</a>
-              </li>
-              <li ClassName="nav-item dropdown">
-                <a ClassName="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
-                </a>
-                <ul ClassName="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li><a ClassName="dropdown-item" href="#">Action</a></li>
-                  <li><a ClassName="dropdown-item" href="#">Another action</a></li>
-                  <li><hr ClassName="dropdown-divider"/></li>
-                  <li><a ClassName="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-              </li>
-              <li ClassName="nav-item">
-                <a ClassName="nav-link disabled">Disabled</a>
-              </li>
-            </ul>
-            <form ClassName="d-flex">
-              <input ClassName="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-              <button ClassName="btn btn-outline-success" type="submit">Search</button>
-            </form>
-          </div>
-        </div>
-      </nav>
-    );
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="/">Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/">Link</a>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a className="dropdown-item" href="/">Action</a></li>
+            <li><a className="dropdown-item" href="/">Another action</a></li>
+            <li><hr className="dropdown-divider"/></li>
+            <li><a className="dropdown-item" href="/">Something else here</a></li>
+          </ul>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled" href="/" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form className="d-flex">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+    </>
+  )
 }
-
